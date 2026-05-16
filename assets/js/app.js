a8f7eddac74e8792d938893e5c8074c172d6c0d0
ERIC GREGORIO DE AQUINO <eric.gaquino@senacsp.edu.br>
2026-05-15 22:36:54 -0300

    frontend

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,14 @@
 const resultado = document.querySelector("#resultado");
+const btnSave = document.querySelector("#btnSave");
+const btnOpenModal = document.querySelector("#btnOpenModal");
+const btnAtualizar = document.querySelector("#btnAtualizar");
+const btnCancelar = document.querySelector("#btnCancelar");
+const modalFormCadastro = document.querySelector("#modalFormCadastro");
+const inputName = document.querySelector('input[id="nome"]');
+const inputEmail = document.querySelector('input[id="email"]');
+const inputTelefone = document.querySelector('input[id="telefone"]');
+const inputCidade = document.querySelector('input[id="cidade"]');
+const inputEstado = document.querySelector('input[id="estado"]');
 
 const api = "http://localhost:5600/";
 
@@ -15,6 +25,9 @@ const showData = async () => {
 
   const showClientes = getAllClientes
     .map((cliente) => {
+      const dataFormatada = new Date(cliente.data_cadastro).toLocaleDateString(
+        "pt-BR",
+      );
       return `
         <div class="row">
           <div class="col">${cliente.nome}</div>
@@ -22,7 +35,7 @@ const showData = async () => {
           <div class="col">${cliente.telefone}</div>
           <div class="col">${cliente.cidade}</div>
           <div class="col">${cliente.estado}</div>
-          <div class="col">${cliente.data_cadastro}</div>
+          <div class="col">${dataFormatada}</div>
           <div>
               <button>Editar</button>
               <button>Deletar</button>
@@ -34,5 +47,36 @@ const showData = async () => {
 
   resultado.innerHTML = showClientes;
 };
+
+btnSave.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const clienteData = {
+    nome: inputName.value,
+    email: inputEmail.value,
+    telefone: inputTelefone.value,
+    cidade: inputCidade.value,
+    estado: inputEstado.value,
+  };
+
+  const result = await fetch(api + "clientes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(clienteData),
+  });
+});
+
+btnOpenModal.addEventListener("click", () => {
+  modalFormCadastro.classList.remove("hide-modal");
+  modalFormCadastro.classList.add("show-modal");
+});
+
+btnCancelar.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalFormCadastro.classList.add("hide-modal");
+  modalFormCadastro.classList.remove("show-modal");
+});
 
 showData();
